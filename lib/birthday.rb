4fe8fa9e4b4d784c2ceefd01a03a4c866084b797
Ricky dildy <rdildy92@gmail.com>
2019-12-26 04:48:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,9 +13,9 @@ end
  def xit    
    age_appropriate_birthday(birthday_kids)
     age_appropriate_birthday.each do |kids_name,age|
-   if age_appropriate_birthday == 27
+   if age_appropriate_birthday == 12
      puts "Happy Birthday #{kids_name}! You are now #{age}"
-     while age_appropriate_birthday == 26 
+     while age_appropriate_birthday == 27
      puts "Happy Birthday #{kids_name}! You are now #{age} years old!\nHappy Birthday #{name}! You are now #{age} years old!\nYou are too old for this."
    end 
  end 
